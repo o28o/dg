1f94433a9ddec3ab5a5c7e3d8994b76e8939d315
o yoga <o28o@mail.ru>
2025-06-01 07:46:32 -0400

    update from server

--- scripts/updateReadPHP.sh
+++ scripts/updateReadPHP.sh
@@ -5,15 +5,16 @@ REFERENCE_FILE="assets/texts/lastupdate_state_file"
 
 # Проверяем, существует ли он
 if [ ! -f "$REFERENCE_FILE" ]; then
-  echo "Файл $REFERENCE_FILE не найден!"
-  exit 1
+newer=""
+else
+newer="-newer $REFERENCE_FILE"
 fi
 
-for i in ` find assets/texts/sutta/ -name "*.json" -newer "$REFERENCE_FILE" | grep -iE "(ru-o.json|experiment|progres)"  | awk -F'/' '{print $NF}' | awk -F'_' '{print $1}' | sort -V`; 
+for i in ` find assets/texts/sutta/ -name "*.json" $newer | grep -iE "(ru-o.json|experiment|progres)"  | awk -F'/' '{print $NF}' | awk -F'_' '{print $1}' | sort -V`; 
 do  
 echo $i
 #grep level5 read.php | grep "$i\""  ; 
-sed -i '/class="level5"/ { /href=.*?q='$i'"/ { /<?php echo \$ifRuLitTrn;?>/! s/<\/span>/<?php echo \$ifRuLitTrn;?><\/span>/; } }' read.php
+sed -i '/class="level5"/ { /href=.*?q='$i'"/ { /<?php echo \$ifRuLitTrn;?>/! s/<\/span>/ <?php echo \$ifRuLitTrn;?><\/span>/; } }' read.php
 done 
  
 
