f329c685acc099c80af8528fe258cc473d6219b2
o yoga <o28o@mail.ru>
2025-06-09 08:40:46 -0400

    update from server

--- read.php
+++ read.php
@@ -218,6 +218,7 @@ if (isset($_GET['q'])) {
        id="paliauto"
        placeholder="e.g. Kāyagat or sn56.11"
        value="<?php echo $q; ?>"
+       style="max-width: 400px;">
   <button type="button" id="clearbtn" class="btn btn-sm ms-1 me-1 rounded-pill">
     <i class="fas fa-times" aria-hidden="true"></i>
     <span class="visually-hidden"><?php echo $clearaption;?></span>
