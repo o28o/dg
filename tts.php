b1a6d1b233cdf869fd88d0724e7f1d283d07eae2
o yoga <o28o@mail.ru>
2025-06-24 23:27:21 -0400

    update from server

--- tts.php
+++ tts.php
@@ -229,14 +229,15 @@ document.addEventListener('DOMContentLoaded', function() {
     </div>
 
     <!-- Form (order 3 на моб, order 2 на десктопе) -->
-    <form id="slugForm" class="d-flex align-items-center flex-nowrap order-3 order-sm-2 flex-grow-1" onsubmit="return goToSlug();" style="min-width: 140px; max-width: 250px;">
-      <input type="text" class="form-control form-control-sm rounded-pill me-1 flex-grow-1" 
-             id="paliauto" name="q" value="<?= htmlspecialchars($slug) ?>" 
-             placeholder="e.g. an3.76" style="min-width: 100px;">
-      <button type="submit" class="btn btn-sm btn-outline-secondary rounded-circle p-1 flex-shrink-0" style="width: 40px;">
-        Go
-      </button>
-    </form>
+<form id="slugForm" class="d-flex align-items-center flex-nowrap order-3 order-sm-2 mx-auto flex-grow-0" onsubmit="return goToSlug();" style="min-width: 140px; max-width: 250px;">
+  <input type="text" class="form-control form-control-sm rounded-pill me-1 flex-grow-1" 
+         id="paliauto" name="q" value="<?= htmlspecialchars($slug) ?>" 
+         placeholder="e.g. an3.76" style="min-width: 100px;">
+  <button type="submit" class="btn btn-sm btn-outline-secondary rounded-circle p-1 flex-shrink-0" style="width: 40px;">
+    Go
+  </button>
+</form>
+
 
   </div>
 </div>
