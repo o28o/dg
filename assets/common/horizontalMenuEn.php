cf3191931b37ca724814ee1091ef3d24eefae4a7
o yoga <o28o@mail.ru>
2025-06-27 16:53:17 -0400

    update from server

--- assets/common/horizontalMenuEn.php
+++ assets/common/horizontalMenuEn.php
@@ -167,7 +167,7 @@ echo '<!--
 <a title="AI-assistants, Dictionaries Pali and Sanskrit" class="dropdown text-decoration-none mx-1 d-md-inline-block" id="MenuDict" data-bs-toggle="dropdown" aria-expanded="false" href="#">
   <figure class="figure d-md-inline-block">
     <i class="menu-icon fa-solid fa-book-atlas"></i>
-    <figcaption class="horiz-menu-item figure-caption text-center">AI and Dicts</figcaption>   
+    <figcaption class="horiz-menu-item figure-caption text-center">AI & Dicts</figcaption>   
   </figure>	  
 </a>
 
